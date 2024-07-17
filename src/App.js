e75ee5102ca6c9761ee644fb9be5d93befe23668
Asmita Kumari <asmitababhanlai@gmail.com>
2024-07-17 14:13:20 +0530

    Working on after signup

--- src/App.js
+++ src/App.js
@@ -1,120 +1,106 @@
 import './App.css';
+import Swal from 'sweetalert2';
 
 function App() {
 
 
 
-  const checkValidation = () => {
+  // const showSwal =()=>{
+  //   Swal.fire({
+  //     title: "Are you sure?",
+  //     text: "You won't be able to revert this!",
+  //     icon: "warning",
+  //     showCancelButton: true,
+  //     confirmButtonColor: "#3085d6",
+  //     cancelButtonColor: "#d33",
+  //     confirmButtonText: "Yes, delete it!"
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
+  //       Swal.fire({
+  //         title: "Deleted!",
+  //         text: "Your file has been deleted.",
+  //         icon: "success"
+  //       });
+  //     }
+  //   });
+  // }
+
+  const showSwal =()=>{
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want to do sign up!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, signup!"
+    }).then((result) => {
+      if (result.isConfirmed) {
     
-    var first = document.getElementById("first").value;
-    var last = document.getElementById("last").value;
-    var email = document.getElementById("email").value;   
-    var password = document.getElementById("password").value;
-    var confirm = document.getElementById("confirm").value;
-    var male  = document.getElementById("male").checked;
-    var female  = document.getElementById("female").checked;
-
-    
-    first = first.trim();
-    last = last.trim();
-    email = email.trim();
-    password = password.trim();
-    confirm = confirm.trim();
-    
-
-   if(!first ||!last ||!email ||!password ||!confirm || (!male && !female)){
-     if(!first && !last && !email && !password && !confirm && (!male && !female)){
-            alert("All fields are mandatory");
-            return false;
-     }
-     if(!first ){
-     alert("Please enter firstname");
-    return false;
-    }
-     if(!last){
-      alert("Please enter lastname");
-      return false;
-    }
-
-    if(!email){
-      alert("Please enter email");
-      return false;
-
-   }
-
-   if(!password){
-    alert("Please enter password ");
-    return false;
-
- }
-
- if(!confirm){
-  alert("Please enter confirm password ");
-  return false;
-
-}
-if(!male && !female){
-  alert("Please tickout gender");
-  return false;
-
-}
-}
-    else{
-    if(password !== confirm){
-      alert("Please fill same password");
-      return false;
-    
-    }
-
-   
-
-   else{
-    alert("All good")
-    return true;
-   
-   }
+      }
+    });
   }
-}
 
+  const checkValidation = () => {
+    const first = document.getElementById("first").value.trim();
+    const last = document.getElementById("last").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const confirm = document.getElementById("confirm").value.trim();
+    const male = document.getElementById("male").checked;
+    const female = document.getElementById("female").checked;
 
+    if (!first || !last || !email || !password || !confirm || (!male && !female)) {
+      let message = "All fields are mandatory";
+      if (!first) message = "Please enter firstname";
+      else if (!last) message = "Please enter lastname";
+      else if (!email) message = "Please enter email";
+      else if (!password) message = "Please enter password";
+      else if (!confirm) message = "Please enter confirm password";
+      else if (!male && !female) message = "Please tick gender";
 
+      alert(message);
+      return false;
+    }
 
+    if (password !== confirm) {
+      alert("Please fill the same password");
+      return false;
+    }
+
+    showSwal();
+    return true;
+  }
 
   return (
-      <div className="container">
+    <div className="container">
       <div className="inner">
-        
-        <label for="first" className='mylabel'> First Name</label>
-        <input type="text" id="first"  className='myinput' placeholder='First Name'/><br/>
+        <label htmlFor="first" className='mylabel'>First Name</label>
+        <input type="text" id="first" className='myinput' placeholder='First Name' /><br/>
 
-        <label for="last" className='mylabel'> Last Name</label>
-        <input type="text" id="last" className='myinput'  placeholder=' Last Name'/><br/>
+        <label htmlFor="last" className='mylabel'>Last Name</label>
+        <input type="text" id="last" className='myinput' placeholder='Last Name' /><br/>
 
-        <label for="email" className='mylabel'>Email Adress</label>
-        <input type="email" id="email" className='myinput' placeholder='Email Adress'/><br/>
+        <label htmlFor="email" className='mylabel'>Email Address</label>
+        <input type="email" id="email" className='myinput' placeholder='Email Address' /><br/>
 
-        <label for="password" className='mylabel'>Password</label>
-        <input type="password" id="password" className='myinput' placeholder='Password'/><br/>
+        <label htmlFor="password" className='mylabel'>Password</label>
+        <input type="password" id="password" className='myinput' placeholder='Password' /><br/>
 
-        <label for="confirm" className='mylabel'>Confirm Password</label>
-        <input type="password" id="confirm" className='myinput' placeholder='Confirm Password'/><br/>
-         
+        <label htmlFor="confirm" className='mylabel'>Confirm Password</label>
+        <input type="password" id="confirm" className='myinput' placeholder='Confirm Password' /><br/>
 
-         <label for="gender" className='mylabel'>Gender</label>
-        
-          <label for="male" className='mylabel1'>Male</label>
-        <input type="radio"  id="male" name="gender" className='myinput1'/>
-        
-        <label for="female" className='mylabel1' >Female</label>
-        <input type="radio"  id="female" name="gender" className='myinput1'/>
+        <label className='mylabel'>Gender</label>
+        <label htmlFor="male" className='mylabel1'>Male</label>
+        <input type="radio" id="male" name="gender" className='myinput1' />
+        <label htmlFor="female" className='mylabel1'>Female</label>
+        <input type="radio" id="female" name="gender" className='myinput1' />
 
-       <div className='mydiv'> <button type="submit" className='mybutton' onClick={()=> checkValidation()}>Submit
-       </button></div>
-        
-
+        <div className='mydiv'>
+          <button type="submit" className='mybutton' onClick={checkValidation}>Submit</button>
+        </div>
+      </div>
     </div>
-    </div>
-
   );
 }
 
